ee57702813690dde783fffd862a1ab7e6084c3f6
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-11-29 18:31:07 +0000

    Added http server for streaming
    - Haven't called response.finish()  yet.

--- watcher.js
+++ watcher.js
@@ -14,8 +14,26 @@ var filename = '/Users/makoto/work/personal/friend_hangman/log/development.log'
 
 // Look at http://nodejs.org/api.html#_child_processes for detail.
 var tail = process.createChildProcess("tail", ["-f", filename]);
+sys.puts("start tailing");
 tail.addListener("output", function (data) {
   sys.puts(data);
 });
-sys.puts("start tailing");
 
+
+// From nodejs.org/jsconf.pdf slide 56
+var http = require("http");
+http.createServer(function(req,res){
+  res.sendHeader(200,{"Content-Type": "text/plain"});
+  res.sendBody("Hel");
+  res.sendBody("lo\r\n");
+  tail.addListener("output", function (data) {
+    res.sendBody(data);
+  });
+  
+  
+  // 
+  // setTimeout(function () {
+  //   res.sendBody("World\r\n");
+  //   res.finish();
+  // }, 2000);
+}).listen(8000);
