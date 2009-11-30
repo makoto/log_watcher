ede1e21b90aff5b2c945cf8367979969c124ff94
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-11-30 23:32:45 +0000

    Changed to comet way
    - Removed if statement to simplify the code
    - Added fu.js

--- watcher.js
+++ watcher.js
@@ -1,4 +1,5 @@
-var sys = require('sys')
+var sys = require('sys');
+var fu = require('./fu');
 var filename = process.ARGV[2];
 
 if (!filename)
@@ -8,45 +9,19 @@ if (!filename)
 var tail = process.createChildProcess("tail", ["-f", filename]);
 sys.puts("start tailing");
 
-tail.addListener("output", function (data) {
-  sys.puts(data);
-});
+fu.listen(8003);
+fu.get("/", fu.staticHandler("index.html"));
+fu.get("/jquery-1.2.6.min.js", fu.staticHandler("jquery-1.2.6.min.js"));
 
-var summary;
-var matched;
-var matched = [];
-var junk;
-var method;
-var ip;
-var date;
-var total;
-var time;
-var view;
-var db;
-// From nodejs.org/jsconf.pdf slide 56
-var http = require("http");
-http.createServer(function(req,res){
+fu.get("/watch", function  (req, res) {
   res.sendHeader(200,{"Content-Type": "text/plain"});
-  res.sendBody("Hel");
-  res.sendBody("lo\r\n");
   tail.addListener("output", function (data) {
-    if (data.search(/Processing/) != -1)
-    {
-      res.sendBody(data);
-      matched = data.match(/Processing (.*)\[/)[1].split(' ');
-      // ResultsController#index (for 127.0.0.1 at 2009-11-30 01:08:51) 
-      method = matched[0];
-    }else if(data.search(/Completed in/) != -1)
-    { 
-      res.sendBody(data);
-
-      matched = data.match(/Completed in (.*)\)/)[1].split(" ");
-      // 40ms (View: 12, DB: 2
-      total = matched[0].replace(/ms/,"");
-      db = matched[2].replace(/,/,"");
-      view = matched[4];
-      res.sendBody("method:" + method + " total:" + total + " db:" + db + " view: " + view );
-      res.sendBody("\n");
-    }
-  });
-}).listen(8000);
+    res.sendBody(data + "\n");
+    
+    // times out after 10 sec 
+    setTimeout(function () {
+      res.sendBody("Finishing");
+      res.finish();
+    }, 10 * 1000);
+  });    
+})
