bda0b7c98cd273d3e96afb41909c0db2ecd07c2c
Makoto Inoue <makoto@new-bamboo.co.uk>
2009-11-30 01:27:30 +0000

    Extracted method, total, db, view

--- watcher.js
+++ watcher.js
@@ -13,7 +13,16 @@ tail.addListener("output", function (data) {
 });
 
 var summary;
-var trimmed_data;
+var matched;
+var matched = [];
+var junk;
+var method;
+var ip;
+var date;
+var total;
+var time;
+var view;
+var db;
 // From nodejs.org/jsconf.pdf slide 56
 var http = require("http");
 http.createServer(function(req,res){
@@ -24,12 +33,19 @@ http.createServer(function(req,res){
     if (data.search(/Processing/) != -1)
     {
       res.sendBody(data);
-      summary = data.match(/Processing (.*)\[/)[1];
+      matched = data.match(/Processing (.*)\[/)[1].split(' ');
+      // ResultsController#index (for 127.0.0.1 at 2009-11-30 01:08:51) 
+      method = matched[0];
     }else if(data.search(/Completed in/) != -1)
     { 
       res.sendBody(data);
-      trimmed_data = data.replace(/^\s+|\s+$/g,"");
-      res.sendBody(summary + data.match(/Completed in (.*)\)/)[1]);
+
+      matched = data.match(/Completed in (.*)\)/)[1].split(" ");
+      // 40ms (View: 12, DB: 2
+      total = matched[0].replace(/ms/,"");
+      db = matched[2].replace(/,/,"");
+      view = matched[4];
+      res.sendBody("method:" + method + " total:" + total + " db:" + db + " view: " + view );
       res.sendBody("\n");
     }
   });
